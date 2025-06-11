d90f0b26f8bd6ccc17d6d683dcc6af29bc5d542d
Maggymoh <159119940+Maggymoh@users.noreply.github.com>
2025-06-11 10:43:40 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('vat-calculator').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
